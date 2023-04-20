7a41476a8c7254f4e31ef32214485b266dc2862a
Luke Herbold <luke@sleeper.app>
2023-04-19 17:21:48 -0700

    Converting socket connections from UDP -> TCP to improve stability and transmital of large context objects.

--- template/index.tsx
+++ template/index.tsx
@@ -1,5 +1,11 @@
 import React, {useCallback, useState} from 'react';
-import {AppRegistry, View, StyleSheet} from 'react-native';
+import {
+  AppRegistry,
+  ActivityIndicator,
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
 import {DevServer, Types} from '@sleeperhq/mini-core';
 
 import App from './src/App';
@@ -9,15 +15,33 @@ DevServer.init(config);
 
 const Template = () => {
   const [context, setContext] = useState<Types.Context>({});
+  const [connected, setConnected] = useState<boolean>(false);
 
   const _onContextChanged = useCallback((data: Types.Context) => {
     setContext(data);
   }, []);
 
+  const _onConnected = useCallback((value: boolean) => {
+    setConnected(value);
+  }, []);
+
+  const _renderWaitingForConnection = () => {
+    return (
+      <View style={styles.loadingContainer}>
+        <Text style={styles.loadingText}>Waiting for connection...</Text>
+        <ActivityIndicator size={50} />
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      {__DEV__ && <DevServer onContextChanged={_onContextChanged} />}
-      <App context={context} />
+      <DevServer
+        onContextChanged={_onContextChanged}
+        onConnected={_onConnected}
+      />
+      {connected && <App context={context} />}
+      {!connected && _renderWaitingForConnection()}
     </View>
   );
 };
@@ -26,6 +50,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#18202f',
+  },
+  loadingText: {
+    color: 'white',
+    fontSize: 20,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
