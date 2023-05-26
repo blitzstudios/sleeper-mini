6938294b47fb4f764f9e5e70e8b7d00749f38b4e
Luke Herbold <luke@sleeper.app>
2023-05-25 23:37:53 -0700

    Adding stub project for our internal hackathon.

---
+++ template/src/Hackathon/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import * as RN from 'react-native';
+import {Types, Sleeper} from '@sleeperhq/mini-core';
+
+type OwnProps = {
+  context: Types.Context;
+};
+
+const Hackathon = (props: OwnProps) => {
+  const {context} = props;
+
+  return (
+    <RN.View style={styles.container}>
+      <Sleeper.Text style={styles.text}>
+        Hello {context?.user?.display_name}!
+      </Sleeper.Text>
+    </RN.View>
+  );
+};
+
+const styles = RN.StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    color: 'white',
+  },
+});
+
+export default Hackathon;
