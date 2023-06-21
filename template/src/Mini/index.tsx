181dab5513b4697003c42a6244f0b833852166cc
Taylor M <taylor@sleeper.app>
2023-06-21 14:24:45 -0600

    Moved

--- template/src/Mini/index.tsx
+++ template/src/Mini/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Types} from '@sleeperhq/mini-core';
-import Videos from './videos';
+import * as RN from 'react-native';
+import {Types, Sleeper, Fonts, Theme} from '@sleeperhq/mini-core';
+
 type OwnProps = {
   context: Types.Context;
 };
@@ -8,7 +9,42 @@ type OwnProps = {
 const Mini = (props: OwnProps) => {
   const {context} = props;
 
-  return <Videos context={context} />;
+  return (
+    <RN.View style={styles.container}>
+      <Sleeper.Text style={styles.text}>
+        Hello {context?.user?.display_name}!
+      </Sleeper.Text>
+      <Sleeper.Text style={styles.text}>
+        Open app.json and select a sample to learn what API features are
+        available.
+      </Sleeper.Text>
+      <Sleeper.Text style={styles.text}>
+        When you're ready to get started, edit this file (src/Mini/index.tsx)
+        and add your own code.
+      </Sleeper.Text>
+      <Sleeper.Text style={styles.text}>
+        Feel free to copy any package from mini_packages.json to this project's
+        package.json. They will be included in your final mini.
+      </Sleeper.Text>
+    </RN.View>
+  );
 };
+
+const styles = RN.StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: Theme.primaryText,
+    padding: 10,
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 10,
+    margin: 5,
+    ...Fonts.Styles.Body1,
+  },
+});
 
 export default Mini;
