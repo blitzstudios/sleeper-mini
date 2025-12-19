b8318fffff9e580437326832ba71deead325123d
Luke Herbold <luke@sleeper.app>
2025-12-19 14:08:12 -0800

    Updating to the new architecture!

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+      'react-native-worklets/plugin',
+  ],
 };
 
